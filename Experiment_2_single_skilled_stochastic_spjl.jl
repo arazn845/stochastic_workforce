404f3031bac08a7b06a7c1fa06bf60bacd6ec8a0
Araz <71676938+arazn845@users.noreply.github.com>
2022-07-02 13:04:18 +1000

    Update Experiment_2_single_skilled_stochastic_spjl.jl

--- Experiment_2_single_skilled_stochastic_spjl.jl
+++ Experiment_2_single_skilled_stochastic_spjl.jl
@@ -1,13 +1,9 @@
 using StochasticPrograms
+using GLPK
 using CPLEX
 using Distributions
 using Plots
-using DataFrames
-using Random
 using LinearAlgebra
-
-###################################
-###################################
 
 Ξ = 1
 H = 12
@@ -92,40 +88,63 @@ d = [
     0.3  0.4  0.9  0.2  0.6  1.0  0.4  0.3  0.4  0.6
 ]
 
-#######################################################
-######################################################
+χᵐᵃˣ = fill(1, H, J)
 
-skill = @stochastic_model begin
 
-    @stage 1 begin
-        @decision(model, ψ[1:H , 1:J], Bin)
-        @decision(model, α[h in 1:H , j in 1:J , t in 1:Tᵈ], Bin) 
+@stochastic_model skill begin
+   @stage 1 begin
+        @decision(skill, ψ[1:H , 1:J], Bin)
+        @decision(skill, 0 ≤ χ[1:H , 1:J])
+        @decision(skill, α[1:H , 1:J , 1:Tᵈ], Bin)
+        @decision(skill, z1[1:H , 1:J , 1:Tᵈ], Bin)
+        @decision(skill, z2[1:H , 1:J , 1:Tᵈ])
         
-        @objective(model, Min, sum(ψ[h,j] * zᵖ[h] * (Tˢ + Tᵈ) for h in 1:H for j in 1:J ) )
+        @objective(skill, Min, sum( zᵖ[h] * ψ[h,j] * (Tᵈ + Tˢ) for h in 1:H for j in 1:J ) +
+                            sum(zᵐ[h,j] * χ[h,j] for h in 1:H for j in 1:J ) 
+        )
 
-        @constraint(model, s1_c1[h in 1:H , j in 1:J], ψ[h,j] ≤ s[h,j])
-        @constraint(model, s1_c2[j in 1:J , t in 1:Tᵈ], sum( α[h,j,t] for h in 1:H ) ≥ d[j,t] )
-        @constraint(model, s1_c3[h in 1:H , j in 1:J , t in 1:Tᵈ], α[h,j,t] ≤ ψ[h,j])
-        @constraint(model, s1_c4[h in 1:H , t in 1:Tᵈ], sum( α[h,j,t] for j in 1:J) ≤ 1 )
+        @constraint(skill, linear1[h = 1:H , j = 1:J , t =1:Tᵈ], z1[h,j,t] ≤ α[h,j,t] )
+        @constraint(skill, linear2[h = 1:H , j = 1:J , t =1:Tᵈ], z1[h,j,t] ≤ ψ[h,j] )
+        @constraint(skill, linear3[h = 1:H , j = 1:J , t =1:Tᵈ], z1[h,j,t] ≥ α[h,j,t] + ψ[h,j] -1 )
+
+        @constraint(skill, linear4[h = 1:H , j = 1:J , t =1:Tᵈ], z2[h,j,t] ≤ α[h,j,t] )
+        @constraint(skill, linear5[h = 1:H , j = 1:J , t =1:Tᵈ], z2[h,j,t] ≤ χ[h,j] )
+        @constraint(skill, linear6[h = 1:H , j = 1:J , t =1:Tᵈ], z2[h,j,t] ≥ α[h,j,t] + χ[h,j] -1 )
+
+        @constraint(skill, demand_met[ j in 1:J , t in 1:Tᵈ ], sum(z1[h,j,t] + z2[h,j,t] for h in 1:H ) ≥ d[j,t] )
+        @constraint(skill, permanent_pool[h in 1:H , j in 1:J], ψ[h,j] ≤ s[h,j] );
+        @constraint(skill, allocation_recruitment_training[h in 1:H , j in 1:J , t in 1:Tᵈ], α[h,j,t] ≤ ψ[h,j] + 10 * χ[h,j] )
+        @constraint(skill, no_more_than_one_station[ h in 1:H , t in 1:Tᵈ ], sum(α[h,j,t] for j in 1:J) ≤ 1 );
+        @constraint(skill, single_skilling_at_recruitment[h in 1:H , j in 1:J], χ[h,j] ≤ ( 1 - ψ[h,j] ) * χᵐᵃˣ[h,j] );
+        @constraint(skill, training_recruited[ h in 1:H ], sum(χ[h,j] for j in 1:J ) ≤ sum(ψ[h,j] for j in 1:J) * J  );
+        
+        
     end
     
-    @stage 2 begin
+    @stage 2 begin 
+        
         @uncertain dξ
-        @recourse(model, αξ[1:H , 1:J , 1:Tˢ], Bin)
-        @recourse(model, 0 ≤ γξ[1:J , 1:Tˢ])
+        @recourse(skill, 0 ≤ γξ[1:J , 1:Tˢ])
+        @recourse(skill, αξ[1:H , 1:J , 1:Tˢ], Bin)
+        @recourse(skill, z3[1:H , 1:J , 1:Tˢ], Bin)
+        @recourse(skill, z4[1:H , 1:J , 1:Tˢ])
+
+        @objective(skill, Min, sum(zᶜ[j] * γξ[j,t] for j in 1:J for t in 1:Tˢ ) )
         
-        @objective(model, Min, sum( zᶜ[j] * γξ[j,t] for j in 1:J for t in 1:Tˢ ) )
+        @constraint(skill, linear7[h = 1:H , j = 1:J , t =1:Tˢ], z3[h,j,t] ≤ αξ[h,j,t] )
+        @constraint(skill, linear8[h = 1:H , j = 1:J , t =1:Tˢ], z3[h,j,t] ≤ ψ[h,j] )
+        @constraint(skill, linear9[h = 1:H , j = 1:J , t =1:Tˢ], z3[h,j,t] ≥ αξ[h,j,t] + ψ[h,j] -1 )
+
+        @constraint(skill, linear10[h = 1:H , j = 1:J , t =1:Tˢ], z4[h,j,t] ≤ αξ[h,j,t] )
+        @constraint(skill, linear11[h = 1:H , j = 1:J , t =1:Tˢ], z4[h,j,t] ≤ χ[h,j] )
+        @constraint(skill, linear12[h = 1:H , j = 1:J , t =1:Tˢ], z4[h,j,t] ≥ αξ[h,j,t] + χ[h,j] -1 )
         
-        @constraint(model, s2_c1[j in 1:J , t in 1:Tˢ], sum(αξ[h,j,t] for h in 1:H) + γξ[j,t] ≥ dξ[j,t])
-        @constraint(model, s2_c2[h in 1:H , j in 1:J , t in 1:Tˢ], αξ[h,j,t] ≤ ψ[h,j] )
-        @constraint(model, s2_c3[h in 1:H , t in 1:Tˢ], sum(αξ[h,j,t] for j in 1:J) ≤ 1 )
+        @constraint(skill, stage2_demand_met[j = 1:J , t =1:Tˢ], sum(z3[h,j,t] for h in 1:H ) + sum(z4[h,j,t] for h in 1:H) + γξ[j,t] ≥ dξ[j,t] )
+        @constraint(skill, stage2_permanent_allocability[h in 1:H , j in 1:J , t in 1:Tˢ], αξ[h,j,t] ≤ ψ[h,j] + 10 * χ[h,j] )
+        @constraint(skill, stage2_no_more_than_one_station[ h in 1:H , t in 1:Tˢ ], sum(αξ[h,j,t] for j in 1:J) ≤ 1 )
     end
-    
 end
 
-
-#########################################################
-#########################################################
 
 @sampler SimpleSampler = begin
     N::MvNormal
@@ -140,25 +159,12 @@ end
 
 Σ = Diagonal(fill(0.1 , J))
 
-#########################################################
-#########################################################
+#######
+
 
 sp = instantiate(skill, SimpleSampler(µ, Σ), Ξ)
 
 set_optimizer(sp, CPLEX.Optimizer)
 
 optimize!(sp)
-
-@show objective_value(sp)
-@show DataFrame( value.(sp[1, :ψ]), :auto ) 
-@show DataFrame( value.(sp[1, :ψ]), :auto ) 
-
-
-
-value.(sp[1, :α])
-
-sp[2 , :γξ]
-
-sum(value.(sp[2 , :γξ], i) for i in 1:10 ) .* (1/ Ξ)
-
 
